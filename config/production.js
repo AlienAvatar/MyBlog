e178bf00ccb2a19457651b270c160c6099a72238
Alien <dxf767808421@163.com>
2019-02-24 20:35:34 +0800

    Add files via upload

---
+++ config/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodb:'mongodb://myblog:<PASSWORD>@cluster0-shard-00-00-cjymn.mongodb.net:27017,cluster0-shard-00-01-cjymn.mongodb.net:27017,cluster0-shard-00-02-cjymn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+};
